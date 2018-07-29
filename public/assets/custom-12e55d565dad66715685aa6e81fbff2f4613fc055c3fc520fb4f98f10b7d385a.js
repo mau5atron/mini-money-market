5d6e0456efa02e49ad9b0816be12fcfe4e3bb6a2
mau5atron <gabrielbetancourt96@outlook.com>
2018-07-28 23:20:09 -0400

    compiled assets + included navbar menu

---
+++ public/assets/custom-12e55d565dad66715685aa6e81fbff2f4613fc055c3fc520fb4f98f10b7d385a.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(){
+	const openNav() = () =>
+	    document.getElementById("myNav").style.width = "100%";
+
+	/* Close when someone clicks on the "x" symbol inside the overlay */
+	const closeNav() = () => 
+	  document.getElementById("myNav").style.width = "0%";
+});
