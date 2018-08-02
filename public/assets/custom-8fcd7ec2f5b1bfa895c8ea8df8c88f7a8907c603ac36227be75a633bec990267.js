1f9f138be5be17e7d3a05cd600189fac484592ab
mau5atron <gabrielbetancourt96@outlook.com>
2018-08-02 10:40:41 -0400

    precompiled assets

---
+++ public/assets/custom-8fcd7ec2f5b1bfa895c8ea8df8c88f7a8907c603ac36227be75a633bec990267.js
@@ -0,0 +1,48 @@
+// call openNav function when myNav is triggered during click
+const openNav = () =>
+    document.getElementById("myNav").style.width = "100%";
+
+// call and close the overlay when someone clicks on the close button that triggers closeNav function
+const closeNav = () => 
+  document.getElementById("myNav").style.width = "0%";
+
+// handles resizing of desktop navbar
+$(document).ready(function(){
+  var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+    
+    if (scrollTop >= 100) {
+      $('#nav-desktop').addClass('scrolled-nav');
+      $('.site-title').css("margin-top", "2rem");
+    } else if (scrollTop < 100) {
+      $('#nav-desktop').removeClass('scrolled-nav');
+    } 
+  }); 
+});
+
+// does not work but keep just in case :3
+// function current_url(){ 
+//   if (window.location.pathname === "/lenders"){
+//      // document.borrower.action = "/lenders";
+//      document.getElementById('borrower_form').style.display = 'none';
+//   } else if(window.location.pathname === "/borrowers"){
+//      document.getElementById('lender_form').style.display = 'none';
+//   }else {
+//   	return;
+//   }
+// } 
+// window.onload = current_url();
+
+// this function only displays one of the forms in according to the path 
+window.onload =	function current_url(){
+									if (window.location.pathname === "/lenders"){
+								     // document.borrower.action = "/lenders";
+								     document.getElementById('borrower_form').style.display = 'none';
+								  } else {
+								     document.getElementById('lender_form').style.display = 'none';
+									}
+								} 
+
+;
